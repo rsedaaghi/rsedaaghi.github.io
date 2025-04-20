ee4a41a31ae93022631a9c6c6776d041a91a871f
Reza Sedaaghi <rsedaaghi@gmail.com>
2025-04-20 11:55:26 +0330

    UPDATE: working with PWA requirements

--- vite.config.js
+++ vite.config.js
@@ -12,28 +12,70 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
-        name: "Reza Sedaaghi Portfolio",
-        short_name: "Portfolio",
+        id: "/", // Provides a computed app id
+        name: "Reza Sedaaghi",
+        short_name: "RSedaaghi",
         description:
           "Portfolio website of Reza Sedaaghi showcasing software development skills and experience.",
-        theme_color: "#1976d2",
-        background_color: "#ffffff",
+        start_url: "/",
         display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
         icons: [
           {
-            src: "/favicon-192x192.png",
+            src: "/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/favicon-512x512.png",
+            src: "/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
-        ],
+        ]
       },
       workbox: {
-        // Increase allowed precache file size to 5 MB (5 * 1024 * 1024)
+        // Increase allowed precache file size to 5 MB
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
     }),
